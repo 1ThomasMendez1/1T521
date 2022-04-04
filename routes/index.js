83ab95cc52ba6deb62c102db591409e5839948bc
1ThomasMendez1 <tmendez@udesa.edu.ar>
2022-04-04 11:54:09 -0300

    Renderizacion de index

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+let indexController = require('../controladores/index.js')
+
+router.get('/', indexController.index)
+module.exports = router;
