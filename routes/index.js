c642632831afd7e48ebdb02fcdb7ba87dffd6a0c
1ThomasMendez1 <tmendez@udesa.edu.ar>
2022-04-19 00:03:31 -0300

    Views,Routes

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-let indexController = require('../controladores/index.js')
-
-router.get('/', indexController.index)
-router.get('/loged', indexController.index)
-
-module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,1 @@
-const express = require('express'); //realizo un require del modulo express
-const router = express.Router();  //variable que guarda la ejecución del método router de express
-let productos = require('../controladores/productsControllers'); //requiero el controlador de productos
 
-
-
-router.get('/detalle', productos.detail)
-router.get('/product-add', productos.productAdd)
-router.get('/search-results', productos.searchResults)
-
-module.exports = router
