7025054477240c4d291be5b97519d6e644da6552
bautisanpedro <90062367+bautisanpedro@users.noreply.github.com>
2022-06-13 11:41:06 -0300

    subiendo database

--- controladores/productsControllers.js
+++ controladores/productsControllers.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-const modulos = require("../db/modulos");
-
-
-
-const productsControllers = {
-    index: function (req, res) {
-      
-        res.render('products', {
-            listaZapas: modulos.productos,
-            listaComentarios: modulos.comentarios,
-            imagen : modulos.addPro
-        })
-    }, 
-
-    productAdd: function (req, res) {
-        res.render('product-add', {
-          usuario : modulos.usuarios
-        })
-    },
-    searchResults: function (req, res) {
-        return res.render('search-results', {
-        })
-    }
-    }
-
-
-=======
 const modulos = require("../db/modulos");
 
 
@@ -53,5 +25,4 @@ const productsControllers = {
     }
 
 
->>>>>>> f9d5ac8de8da507128fa54f38051a7c3f8c1c046
 module.exports = productsControllers; 
